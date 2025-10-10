b1b43cc33f946b8c0fd32ac40e723a965c49651a
AjayJoshi99 <ajayjoshi1908@gmail.com>
2025-10-10 15:03:11 +0530

    Solved deployment error in Exam.js

--- learndesk/src/componets/Exam.js
+++ learndesk/src/componets/Exam.js
@@ -1,4 +1,4 @@
-import { useState, navigate } from 'react';
+import { useState } from 'react';
 // import { useLocation } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import data from './ExamData.json';
